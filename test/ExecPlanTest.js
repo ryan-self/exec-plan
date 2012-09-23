5ad0470e2d3e640e0525cacd8d58d88b44e59111
Ryan <stephen.ryan.self@gmail.com>
2012-09-22 17:16:34 -0700

    Modify ExecPlan test cases to take advantage of nodeunit grouping mechanism.

--- test/ExecPlanTest.js
+++ test/ExecPlanTest.js
@@ -13,33 +13,34 @@ var isArray = utils.isArray;
 
 /* --- TEST CASES --- */
 
-// verification of correct construction
-exports['Passing in no constructor config object makes autoPrintOut and autoPrintErr true'] =
-        function (test) {
-    var execPlan = new ExecPlan();
-    test.ok(execPlan.willAutoPrintOut());
-    test.ok(execPlan.willAutoPrintErr());
-    test.done();
-};
-
-exports['Constructor config {autoPrintOut:false} sets autoPrintOut and autoPrintErr accordingly'] =         function (test) {
-    var execPlan = new ExecPlan({autoPrintOut: false});
-    test.equal(execPlan.willAutoPrintOut(), false, 'execPlan will not auto print stdout');
-    test.ok(execPlan.willAutoPrintErr(), 'execPlan will auto print stderr');
-    test.done();
-};
-
-exports['Constructor config {autoPrintErr:false} sets autoPrintOut and autoPrintErr accordingly'] =         function (test) {
-    var execPlan = new ExecPlan({autoPrintErr: false});
-    test.ok(execPlan.willAutoPrintOut(), 'execPlan will auto print stderr');
-    test.equal(execPlan.willAutoPrintErr(), false, 'execPlan will not auto print stderr');
-    test.done();
-};
-
-exports['Constructor config {autoPrintOut:false, autoPrintErr:false} sets autoPrintOut and ' +
-        'autoPrintErr accordingly'] = function (test) {
-    var execPlan = new ExecPlan({autoPrintOut: false, autoPrintErr: false});
-    test.equal(execPlan.willAutoPrintOut(), false, 'execPlan will not auto print stdout');
-    test.equal(execPlan.willAutoPrintErr(), false, 'execPlan will not auto print stderr');
-    test.done();
+// GROUP: verification of correct construction
+exports.construction = {
+    'Passing in no config object makes autoPrintOut/Err true':
+            function (test) {
+        var execPlan = new ExecPlan();
+        test.ok(execPlan.willAutoPrintOut());
+        test.ok(execPlan.willAutoPrintErr());
+        test.done();
+    },
+    'Config {autoPrintOut:false} sets autoPrintOut/Err accordingly':
+            function (test) {
+        var execPlan = new ExecPlan({autoPrintOut: false});
+        test.equal(execPlan.willAutoPrintOut(), false, 'execPlan will not auto print stdout');
+        test.ok(execPlan.willAutoPrintErr(), 'execPlan will auto print stderr');
+        test.done();
+    },
+    'Config {autoPrintErr:false} sets autoPrintOut/Err accordingly':
+            function (test) {
+        var execPlan = new ExecPlan({autoPrintErr: false});
+        test.ok(execPlan.willAutoPrintOut(), 'execPlan will auto print stderr');
+        test.equal(execPlan.willAutoPrintErr(), false, 'execPlan will not auto print stderr');
+        test.done();
+    },
+    'Config {autoPrintOut:false, autoPrintErr:false} sets autoPrintOut/Err correctly':
+            function (test) {
+        var execPlan = new ExecPlan({autoPrintOut: false, autoPrintErr: false});
+        test.equal(execPlan.willAutoPrintOut(), false, 'execPlan will not auto print stdout');
+        test.equal(execPlan.willAutoPrintErr(), false, 'execPlan will not auto print stderr');
+        test.done();
+    }
 };
